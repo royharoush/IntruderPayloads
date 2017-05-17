921def5e75da2bd926f4f76ffeeb4a8cc735332b
royharoush <roy@haroush.net>
2017-05-17 17:18:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,4 +18,6 @@ ln -s /usr/share/golismero/wordlist /opt/wlists/golismero
 ln -s /usr/share/dict /opt/wlists/dict 
 printf "ls /opt/wlists -R | awk '/:$/&&f{s=$0;f=0}/:$/&&!f\{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep -i --color $1" > /usr/bin/wlist-find
 wget https://raw.githubusercontent.com/royharoush/rtools/master/wlist-find -O /usr/bin/wlist-find && chmod +x /usr/bin/wlist-find
+wget https://raw.githubusercontent.com/royharoush/rtools/master/wlist-update -O /usr/bin/wlist-update  && chmod +x /usr/bin/wlist-update
 echo "finished creating wlists, use wlist-find to search for a wordlist to use "
+echo "to update run wlist-update" 
